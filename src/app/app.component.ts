6b444b68689becd62cc0698640913ce63cf0667d
Vinson Fernandez <geekwise.vinson.fernandez@gmail.com>
2019-07-10 18:56:32 -0700

    created all components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'face-off';
+
+  public getRandom() {
+
+    const randomNumber = Math.random() * 100;
+    console.log(randomNumber);
+  }
 }
---
+++ src/app/shared/feed/feed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'geekwise-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.scss']
+})
+export class FeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
